9c18c3c6dd2549a440c35e8a46eb938e2de5dba2
EngineerSmith <pasmith.bus@gmail.com>
2021-05-31 00:16:45 +0100

    Added window.lua, acts as an middleman parent to make the system more simple

---
+++ base/window.lua
@@ -0,0 +1,30 @@
+local path = (...):match("(.-)[^%.]+$")
+local ui = require(path.."ui")
+
+local window = setmetatable({}, ui)
+window.__index = window
+
+window.new = function(width, height)
+    local self = setmetatable(ui(0,0,1,1), window)
+    self:setDimensions(width, height)
+    return self
+end
+
+window.setDimensions = function(self, width, height)
+    if not width then error("Window: 2nd argument required") end
+    if width < 0 then error("Window: 2nd argument must be greater than 0\n Gave: "..width) end
+    
+    if not height then error("Window: 3rd argument required") end
+    if height < 0 then error("Window: 3rd argument must be greater than 0\n Gave: "..height) end
+    
+    self.width = width
+    self.height = height
+end
+
+window.update = function(self, dt)
+    self:updateTransform()
+    self:updateElement(dt)
+    self:updateChildren(dt)
+end
+
+return window
