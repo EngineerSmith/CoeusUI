e2ae8c713968f97676fa1c554f3261dee00ea316
EngineerSmith <pasmith.bus@gmail.com>
2021-05-31 21:35:35 +0100

    Added safe area offset functionality for window element

--- base/window.lua
+++ base/window.lua
@@ -6,7 +6,7 @@ window.__index = window
 
 window.new = function(width, height)
     local self = setmetatable(ui.new(0,0,1,1), window)
-    self:setDimensions(width, height)
+    self:setSafeArea(0, 0, width, height)
     self:draw()
     return self
 end
@@ -22,10 +22,32 @@ window.setDimensions = function(self, width, height)
     self.height = height
 end
 
+window.setSafeOffset = function(self, x, y)
+    if not x then error("Window: 2nd argument required") end
+    if not y then error("Window: 3rd argument required") end
+    
+    self.x = x
+    self.y = y
+end
+
+window.setSafeArea = function(self, x, y, width, height)
+    self:setDimensions(width, height)
+end
+
 window.update = function(self, dt)
     self:updateTransform()
     self:updateElement(dt)
     self:updateChildren(dt)
+end
+
+window.draw = function(self)
+    if self.enabled then
+        love.graphics.push()
+        love.graphics.translate(self.x, self.y)
+        self:drawElement()
+        self:drawChildren()
+        love.graphics.pop()
+    end
 end
 
 return window
